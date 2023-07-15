b8321550ff0d55293881029588c630d30199327a
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-07-15 20:47:01 +0600

    loaded Category Data From dragon server

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+    // console.log(categories, 5)
+
+    useEffect(() => {
+        fetch('http://localhost:3000/categories')
+            .then(res => res.json())
+            .then(data => {
+                setCategories(data)
+            })
+    }, [])
+
     return (
         <div>
-            <h2>Left Nav</h2>
+            <h4>All Category</h4>
+            <div className='ps-4 '>
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/category/${category.id}`} className='text-decoration-none text-secondary fs-5 nav-link active'>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
