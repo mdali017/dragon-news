87af03409f9cc858423814e7c6443f624e2acb5a
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-08-08 21:58:48 +0600

    set local to server

--- src/Pages/Shared/LeftNav/LeftNav.jsx
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import LeftBottom from './LeftBottom/LeftBottom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import LeftBottom from "./LeftBottom/LeftBottom";
 
 const LeftNav = () => {
-    const [categories, setCategories] = useState([]);
-    // console.log(categories, 5)
+  const [categories, setCategories] = useState([]);
+  // console.log(categories, 5)
 
-    useEffect(() => {
-        fetch('http://localhost:3000/categories')
-            .then(res => res.json())
-            .then(data => {
-                setCategories(data)
-            })
-    }, [])
+  useEffect(() => {
+    fetch("https://dragon-news-server-teal-kappa.vercel.app/categories")
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h4>All Category</h4>
-            <div className='ps-4 '>
-                {
-                    categories.map(category => <p key={category.id}>
-                        <Link to={`/category/${category.id}`} className='text-decoration-none text-secondary fs-5 nav-link active'>{category.name}</Link>
-                    </p>)
-                }
-            </div>
-            <LeftBottom></LeftBottom>
-        </div>
-    );
+  return (
+    <div>
+      <h4>All Category</h4>
+      <div className="ps-4 ">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link
+              to={`/category/${category.id}`}
+              className="text-decoration-none text-secondary fs-5 nav-link active"
+            >
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
+      <LeftBottom></LeftBottom>
+    </div>
+  );
 };
 
 export default LeftNav;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,4 @@
-import {
-  Navigate,
-    createBrowserRouter,
-    
-  } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Category from "../Pages/Home/Category/Category";
@@ -13,57 +9,67 @@ import Login from "../Pages/Login/Login/Login";
 import Register from "../Pages/Login/Register/Register";
 import PrivateRoute from "./PrivateRoute";
 
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <LoginLayout></LoginLayout>,
-      children: [
-        {
-          path: '/',
-          element: <Navigate to='/category/0'></Navigate>
-        },
-        {
-          path: 'login',
-          element: <Login></Login>
-        },
-        {
-          path: 'register',
-          element: <Register></Register>
-        }
-      ]
-    },
-    {
-      // path: "/",
-      path: "category",
-      element: <Main></Main>,
-      children: [
-        // {
-        //     path: '/',
-        //     element: <Home></Home>
-        // },
-        // {
-        //     path: '/',
-        //     element: <Category></Category>,
-        //     loader: () => fetch('http://localhost:3000/news')
-        // },
-        {
-          path: ':id',
-          element: <Category></Category>,
-          loader: ({params}) => fetch(`http://localhost:3000/categories/${params.id}`)
-        },
-      ]
-    },
-    {
-      path: '/news',
-      element: <NewsLayout></NewsLayout>,
-      children: [
-        {
-          path: ':id',
-          element: <PrivateRoute><News></News></PrivateRoute>,
-          loader: ({params}) => fetch(`http://localhost:3000/news/${params.id}`)
-        }
-      ]
-    }
-  ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginLayout></LoginLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/category/0"></Navigate>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+  {
+    // path: "/",
+    path: "category",
+    element: <Main></Main>,
+    children: [
+      // {
+      //     path: '/',
+      //     element: <Home></Home>
+      // },
+      // {
+      //     path: '/',
+      //     element: <Category></Category>,
+      //     loader: () => fetch('https://dragon-news-server-teal-kappa.vercel.app/news')
+      // },
+      {
+        path: ":id",
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(
+            `https://dragon-news-server-teal-kappa.vercel.app/categories/${params.id}`
+          ),
+      },
+    ],
+  },
+  {
+    path: "/news",
+    element: <NewsLayout></NewsLayout>,
+    children: [
+      {
+        path: ":id",
+        element: (
+          <PrivateRoute>
+            <News></News>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://dragon-news-server-teal-kappa.vercel.app/news/${params.id}`
+          ),
+      },
+    ],
+  },
+]);
 
-  export default router;
+export default router;
